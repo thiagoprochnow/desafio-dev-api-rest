9bd828629c71bf590db1877e5f0b147bbc1ec100
Thiago Henrique Prochnow <thiprochnow@gmail.com>
2021-09-08 10:42:15 -0300

    Initial Commit

---
+++ src/test/java/br/com/gs/operation/DesafioDevApiRestApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.gs.operation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DesafioDevApiRestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
