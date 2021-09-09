e47564fa0793e856bbcd10afe7189c9bc7fcff37
Thiago Henrique Prochnow <thiprochnow@gmail.com>
2021-09-09 15:48:10 -0300

    chore: configure swagger2 for endpoints documentation

--- src/main/java/br/com/desafio/DesafioDevApiRestApplication.java
+++ src/main/java/br/com/desafio/DesafioDevApiRestApplication.java
@@ -1,4 +1,4 @@
-package br.com.gs.operation;
+package br.com.desafio;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- src/test/java/br/com/desafio/DesafioDevApiRestApplicationTests.java
+++ src/test/java/br/com/desafio/DesafioDevApiRestApplicationTests.java
@@ -1,4 +1,4 @@
-package br.com.gs.operation;
+package br.com.desafio;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
