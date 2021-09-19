69658e0a9966315c94298dfb5788953252ecec14
Thiago Henrique Prochnow <thiprochnow@gmail.com>
2021-09-19 18:03:35 -0300

    docs: add description of endpoints on Swagger

--- src/main/java/br/com/desafio/application/account/AccountController.java
+++ src/main/java/br/com/desafio/application/account/AccountController.java
@@ -14,6 +14,7 @@ import org.springframework.web.bind.annotation.RestController;
 import br.com.desafio.domain.account.entity.Account;
 import br.com.desafio.domain.account.event.AccountService;
 import br.com.desafio.domain.transaction.entity.Transaction;
+import io.swagger.annotations.ApiOperation;
 
 @RestController
 @RequestMapping(value = "/v1/account")
@@ -22,30 +23,35 @@ public class AccountController {
 	@Autowired
 	private AccountService accountService;
 	
+	@ApiOperation(value = "Create a account with givin information.")
 	@RequestMapping(value = "/createAccount", method = RequestMethod.POST)
 	public ResponseEntity<Account> createAccountEndpoint(@RequestBody Account accountBody) throws Exception {
 		Account createdAccount = accountService.createAccount(accountBody);
 		return ResponseEntity.ok(createdAccount);
 	}
 	
+	@ApiOperation(value = "Get a account information.")
 	@RequestMapping(value = "/getAccount", method = RequestMethod.GET)
 	public ResponseEntity<Account> getAccountEndpoint(@RequestParam long idConta) throws Exception {
 		Account account = accountService.getAccount(idConta);
 		return ResponseEntity.ok(account);
 	}
 	
+	@ApiOperation(value = "Block a existing account.")
 	@RequestMapping(value = "/blockAccount", method = RequestMethod.POST)
 	public ResponseEntity<Account> blockAccountEndpoint(@RequestBody Account accountBody) throws Exception {
 		Account account = accountService.blockAccount(accountBody);
 		return ResponseEntity.ok(account);
 	}
 	
+	@ApiOperation(value = "Get all transactions of a account.")
 	@RequestMapping(value = "/getTransactions", method = RequestMethod.GET)
 	public ResponseEntity<List<Transaction>> getTransactionsEndpoint(@RequestParam long idConta) throws Exception {
 		List<Transaction> transactions = accountService.getTransactions(idConta);
 		return ResponseEntity.ok(transactions);
 	}
 	
+	@ApiOperation(value = "Get all transactions of a account in a period range.")
 	@RequestMapping(value = "/getTransactionsByPeriod", method = RequestMethod.GET)
 	public ResponseEntity<List<Transaction>> getTransactionsByPeriodEndpoint(@RequestParam long idConta, @RequestParam Date periodStart, @RequestParam Date periodEnd) throws Exception {
 		List<Transaction> transactions = accountService.getTransactionsByPeriod(idConta, periodStart, periodEnd);
--- src/main/java/br/com/desafio/application/transaction/TransactionController.java
+++ src/main/java/br/com/desafio/application/transaction/TransactionController.java
@@ -11,6 +11,7 @@ import br.com.desafio.domain.account.entity.Account;
 import br.com.desafio.domain.account.event.AccountService;
 import br.com.desafio.domain.transaction.entity.Transaction;
 import br.com.desafio.domain.transaction.event.TransactionService;
+import io.swagger.annotations.ApiOperation;
 
 @RestController
 @RequestMapping(value = "/v1/transaction")
@@ -19,12 +20,14 @@ public class TransactionController {
 	@Autowired
 	private TransactionService transactionService;
 	
+	@ApiOperation(value = "Make a deposit in a account and updates account information with it.")
 	@RequestMapping(value = "/makeDeposit", method = RequestMethod.POST)
 	public ResponseEntity<Account> makeDepositEndpoint(@RequestBody Transaction transactionBody) throws Exception {
 		Account account = transactionService.makeDeposit(transactionBody);
 		return ResponseEntity.ok(account);
 	}
 	
+	@ApiOperation(value = "Make a withdraw in a account and updates account information with it.")
 	@RequestMapping(value = "/makeWithdraw", method = RequestMethod.POST)
 	public ResponseEntity<Account> makeWithdrawEndpoint(@RequestBody Transaction transactionBody) throws Exception {
 		Account account = transactionService.makeWithdraw(transactionBody);
